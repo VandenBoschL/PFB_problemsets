f4111109ff15bd224d0881946fcb98bb73b78003
pfb05 <pfb05@info05.cshl.edu>
2023-10-17 10:59:30 -0400

    Updated first_script

--- python_1/first_script.py
+++ python_1/first_script.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
+############################
+### Python Problem Set 1 ###
+############################
+
+# Working with print(), strings, sys
+
+# Final usage: first_script.py name color activity animal
+# Input your name and your favorites from each category (one object each)
+
 import sys
+
+# Input variable names
+
 input_name = sys.argv[1]
 input_color = sys.argv[2]
 input_activity = sys.argv[3]
 input_animal = sys.argv[4]
+
+# Print out your information
+
 print(f"My name: {input_name}")
 print(f"My favorite color: {input_color}")
 print(f"My favorite activity: {input_activity}")
 print(f"My favorite animal: {input_animal}")
+
+
+## You did it! Great Job!
