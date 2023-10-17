4de00002bfa96a63f898a9688207413e2ed9492d
pfb05 <pfb05@info05.cshl.edu>
2023-10-17 10:55:10 -0400

    Updated first_script

--- python_1/first_script.py
+++ python_1/first_script.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 import sys
 input_name = sys.argv[1]
-print("My name:", input_name)
+input_color = sys.argv[2]
+input_activity = sys.argv[3]
+input_animal = sys.argv[4]
+print(f"My name: {input_name}")
+print(f"My favorite color: {input_color}")
+print(f"My favorite activity: {input_activity}")
+print(f"My favorite animal: {input_animal}")
